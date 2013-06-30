17a97e0759a677a037f8714be5a7c51a03a649df
Peter Hickey <peter.hickey@gmail.com>
2013-06-30 20:54:35 +1000

    Removed refernces to XS-tag since these aren't required. Added (very rough) notes explaining FLAGs for paired-end reads aligned with Bismark.

--- correct_Bismark_PE_SAM.py
+++ correct_Bismark_PE_SAM.py
@@ -8,7 +8,6 @@ import pysam
 ## See accompanying Word document "Paired_end_read_orientation.docx" for details.
 
 ## TODO: Add function to correct single-end data
-## TODO: Do I need XS-tag? If so, change XS-tag to XI-tag so it doesn't clash with Bowtie2's XS-tag
 
 ###### WARNING - CURRENTLY ONLY HANDLE DIRECTIONAL-LIBRARIES ########
 
@@ -23,19 +22,17 @@ args = parser.parse_args()
 # Open the old SAM/BAM file
 OLD = pysam.Samfile(args.infile)
 
-#### Standard Bismark FLAG values
+#### Standard Bismark FLAG values ####
 #  67 - read paired, read mapped in proper pair, first in pair
 # 131 - read paired, read mapped in proper pair, second in pair
 # 115 - read paired, read mapped in proper pair, read reverse strand, mate reverse strand, first in pair
 # 179 - read paired, read mapped in proper pair, read reverse strand, mate reverse strand, second in pair
-####
 
-#### SAM-spec-compliant FLAG values
+#### SAM-spec-compliant FLAG values ####
 #  99 - read paired, read mapped in proper pair, mate reverse strand, first in pair
 # 147 - read paired, read mapped in proper pair, read reverse strand, second in pair
 #  83 - read paired, read mapped in proper pair, read reverse strand, first in pair
 # 163 - read paired, read mapped in proper pair, mate reverse strand, second in pair
-####
 
 # Create new SAM/BAM file
 header = OLD.header
@@ -51,16 +48,12 @@ for read in OLD:
     read.qname = read.qname.rstrip('/[1-2]') # Remove the /1 or /2 suffix attached by Bowtie/Bismark to paired-end reads
     if read.flag == 67L and read.opt('XR') == 'CT' and read.opt('XG') == 'CT':
         read.flag = 99L
-        read.tags = read.tags + [('XS', 'OT')]
     elif read.flag == 131L and read.opt('XR') == 'GA' and read.opt('XG') == 'CT':
         read.flag = 147L
-        read.tags = read.tags + [('XS', 'OT')]
     elif read.flag == 115L and read.opt('XR') == 'CT' and read.opt('XG') == 'GA':
         read.flag = 83L
-        read.tags = read.tags + [('XS', 'OB')]
     elif read.flag == 179L and read.opt('XR') == 'GA' and read.opt('XG') == 'GA':
         read.flag = 163L
-        read.tags = read.tags + [('XS', 'OB')]
     else:
         sys.exit('Error: Unexpected FLAG/XR/XG combination. Is your sample non-directional? This script only works with for directional-librarires.')
     NEW.write(read)
